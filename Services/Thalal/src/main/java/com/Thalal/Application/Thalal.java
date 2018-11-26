f8fabc0d9051ed48919236fd1a1932ad39abbfac
apelavarasan <apelavarasan@hethi.com>
2018-11-26 17:23:23 +0530

    added test commit in branch 1

--- Services/Thalal/src/main/java/com/Thalal/Application/Thalal.java
+++ Services/Thalal/src/main/java/com/Thalal/Application/Thalal.java
@@ -10,7 +10,7 @@ public class Thalal {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Thalal.class, args);
-		System.out.println("Thalal Starting");
+		System.out.println("Thalal Starting adfa adf");
 	}
 	
 }
